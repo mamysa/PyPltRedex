8359cee64c1f1ccbd7864bf0fb9769363f7aae36
mamysa <alexmamyshev@fastmail.com>
2020-02-05 19:04:11 +0000

    better parsing

--- src/parser.py
+++ src/parser.py
@@ -1,16 +1,5 @@
 import enum
-
-def is_lpar(c):
-    return c == '('
-
-def is_rpar(c):
-    return c == ')'
-
-def is_reserved(c): 
-    return c in ['(', ')', '[', ']', '{', '}', '\"', '\'', '`', ';', '#', '|', '\\']
-
-def is_parenthesis(c):
-    return c in ['(', ')', '[', ']', '{', '}']
+import re
 
 def is_whitespace(c):
     return c == ' ' or c == '\t' or c == '\n' or c == '\r'
@@ -18,37 +7,17 @@ def is_whitespace(c):
 def is_newline(c):
     return c == '\n'
 
-def is_digit(c):
-    return c >= '0' and c <= '9'
+def is_reserved(c): 
+    return c in ['(', ')', '[', ']', '{', '}', '\"', '\'', '`', ';', '#', '|', '\\']
 
-def is_ident(string):
-    isnum = True
-    for c in string: 
-        if not is_digit(c):
-            isnum = False
-        if is_reserved(c):
-            return False
-    return not isnum
-
-def is_string(string):
-    return string[0] == '\"' and string[-1] == '\"'
-
-#FIXME need to support floating point numbers and fractions later.
-def is_number(string):
-    for c in string:
-        if not is_digit(c):
-            return False
-    return True
-
-def is_boolean(string):
-    return string in ['#t', '#f']
-
-
-
-class LiteralKind(enum.Enum):
-    Number = 0
-    String = 1
-    Boolean = 2
+class TokenKind(enum.Enum):
+    Integer = 0
+    Decimal = 1
+    String  = 2
+    Boolean = 3
+    Ident   = 4
+    LParen  = 5
+    RParen  = 6
 
 class Literal:
     def __init__(self, value, kind):
@@ -60,207 +29,207 @@ class Literal:
 
 #print( repr(Literal("343", LiteralKind.Number)))
 
-# Essentially is reimplementation of redex_spec.split()  but split is not adequate enough 
-# for comments and literal strings.
-# In case of comments, discard all tokens until the newline.
-# In case of string literals, consume all characters until closing double quote is found.
-class RedexSpecTokenizer:
-    def __init__(self, filename):
-        f = open(filename, 'r')
-        self.buf = f.read()
-        f.close()
-        self.tokens = []
-        # character offsets.
-        self.start = 0
-        self.end = 0
-
-    def peek(self):
-        if self.end >= len(self.buf):
-            return '\0'
-        return self.buf[self.end]
-
-    def advance(self):
-        self.end += 1
-
-    def get(self):
-        token = self.buf[self.start:self.end]
-        self.start = self.end
-        return token
-
-    def tokenize_string(self):
-        self.advance()
-        while self.peek() != '\"':
-            self.advance()
-
-            if self.peek() == '\0':
-                assert False, 'reached EOF while tokenizing a string'
-
-        self.advance()
-        self.tokens.append(self.get())
-
-    def tokenize_identifier(self):
-        while not is_whitespace(self.peek()) and not is_reserved(self.peek()):
-            self.advance()
-            if self.peek() == '\0':
-                break
-        self.tokens.append(self.get())
-
-    def skip_comment(self):
-        self.advance()
-        while not is_newline(self.peek()):
-            self.advance()
-            if self.peek() == '\0':
-                break
-        self.advance()
-        self.get()
-
-    def tokenize(self):
-        while self.peek() != '\0':
-
-            char = self.peek()
-            if is_parenthesis(char):
-                self.advance()
-                self.tokens.append(self.get())
-                continue
-
-            if char == '\"': 
-                self.tokenize_string()
-                continue
-
-            if char == ';':
-                self.skip_comment()
-                continue
-
-            if is_whitespace(char):
-                self.advance()
-                self.get()
-                continue
-
-            self.tokenize_identifier()
-
-        return self.tokens
-
-
-print( RedexSpecTokenizer('tok').tokenize())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class RedexSpecParser:
-    def __init__(self, filename):
-        f = open(filename, 'r')
-        self.buf = f.read()
-        self.cursor = 0
-        self.buf = self.buf.replace('(', ' ( ') \
-                           .replace(')', ' ) ') \
-                           .split()
-        #print(self.buf)
-        f.close()
 
+    # Essentially is reimplementation of redex_spec.split()  but split is not adequate enough 
+    # for comments and literal strings.
+    # In case of comments, discard all tokens until the newline.
+    # In case of string literals, consume all characters until closing double quote is found.
+    # FIXME perhaps shouldn't use \0 to indicate eof?
+    class RedexSpecTokenizer:
+        def __init__(self, filename):
+            f = open(filename, 'r')
+            self.buf = f.read()
+            f.close()
+            # character offsets.
+            self.start = 0
+            self.end = 0
+
+            # Order sensitive, Ident may also match integers and decimals.
+            # ^ and $ anchor to the beginning and end. 
+            # Identifiers must not contain the following reserved characters:
+            # ( ) [ ] { } " , ' ` ; # | \
+            self.matchers = {
+                TokenKind.Integer : re.compile('^-?[0-9]+$'),
+                TokenKind.Decimal : re.compile('^-?[0-9]*\.[0-9]+$'),
+                TokenKind.Boolean : re.compile('^(#t|#f)$'),
+                TokenKind.Ident   : re.compile('^([^\(\)\[\]{}\"\'`;#|\\\])+$'),
+            }
+
+        def advance(self):
+            self.end += 1
+
+        def peek(self):
+            if self.end >= len(self.buf):
+                return '\0'
+            return self.buf[self.end]
+
+        def extract(self):
+            token = self.buf[self.start:self.end]
+            self.start = self.end
+            return token
+
+        def next(self):
+            while self.peek() != '\0':
+
+                # skip whitespace
+                char = self.peek()
+                if is_whitespace(char):
+                    self.advance()
+                    self.extract()
+                    continue
+                
+                # comments
+                if char == ';':
+                    self.advance()
+                    while not is_newline(self.peek()):
+                        self.advance()
+                        if self.peek() == '\0':
+                            break
+                    self.advance()
+                    self.extract()
+                    continue
+
+                # various parentheses / braces / etc
+                if char in ['(', '[', '{']:
+                    self.advance()
+                    return (TokenKind.LParen, self.extract())
+
+                if char in [')', ']', '}']:
+                    self.advance()
+                    return (TokenKind.RParen, self.extract())
+
+                # string literal
+                if char == '\"': 
+                    self.advance()
+                    while self.peek() != '\"':
+                        self.advance()
+                        if self.peek() == '\\':
+                            self.advance()
+                            self.advance()
+                        if self.peek() == '\0':
+                            assert False, 'reached EOF while tokenizing a string'
+                    self.advance()
+                    return (TokenKind.String, self.extract())
+
+                # otherwise, read until the next whitespace and identify the token 
+                # can be a number, identifier, etc.
+                # TODO confused as to what #lang is. 
+                while not (is_whitespace(self.peek()) or is_reserved(self.peek())) :
+                    self.advance()
+                    if self.peek() == '\0':
+                        break
+                tok = self.extract()
+
+                for kind, matcher in self.matchers.items():
+                    if matcher.match(tok): 
+                        return (kind, tok)
+                assert False, 'unknown token ' + tok
+            return None
+    
+    def __init__(self, filename):
+        self.tokenizer = self.RedexSpecTokenizer(filename)
+        self.nexttoken = self.tokenizer.next()
 
     def peek(self):
-        if self.cursor < len(self.buf):
-            return self.buf[self.cursor]
+        if self.nexttoken == None:
+            assert False, 'reached EOF'
+        return self.nexttoken[0]
         assert False, 'unexpected eof'
 
-    def expect(self, tok):
-        if self.cursor < len(self.buf) and self.buf[self.cursor] == tok:
-            self.cursor += 1
-            return
+    def peekv(self):
+        if self.nexttoken == None:
+            assert False, 'reached EOF'
+        return self.nexttoken
+        assert False, 'unexpected eof'
+
+    def expect(self, kind, tok=None):
+        if self.nexttoken == None:
+            assert False, 'reached EOF'
+
+        if self.nexttoken[0] == kind:
+            if tok != None:
+                if self.nexttoken[1] == tok: 
+                    ret = self.nexttoken
+                    self.nexttoken = self.tokenizer.next()
+                    return ret[1]
+                else:
+                    assert False, 'unexpected ' + tok
+            else:
+                ret = self.nexttoken
+                self.nexttoken = self.tokenizer.next()
+                return ret[1]
         assert False, 'unexpected ' + tok
-
-    def consume_ident(self):
-        isnum = True
-        for c in self.buf[self.cursor]:
-            if not is_digit(c):
-                isnum = False
-            if is_reserved(c):
-                assert False, 'contains reserved char'
-        if isnum:
-            assert False, 'is number'
-
-        tok = self.buf[self.cursor]
-        self.cursor += 1
-        return tok
-
-
-
-
-
 
     # (define-language lang-name non-terminal-def ...)
     def define_language(self):
-        self.expect('define-language')
-        lang_name = self.consume_ident()
+        self.expect(TokenKind.Ident, 'define-language')
+        lang_name = self.expect(TokenKind.Ident)
+
         nts = []
         nts.append(self.non_terminal_def())
 
-        while self.peek() != ')':
+        while self.peek() != TokenKind.RParen:
             nts.append(self.non_terminal_def())
-        self.expect(')')
+        self.expect(TokenKind.RParen)
         return (lang_name, nts)
 
     # non-terminal-def = (non-terminal-name ::= pattern ...+)
     def non_terminal_def(self):
-        self.expect('(')
-        not_terminal_name = self.consume_ident()
-        self.expect('::=')
+        self.expect(TokenKind.LParen)
+        not_terminal_name = self.expect(TokenKind.Ident) 
+        self.expect(TokenKind.Ident, '::=')
 
         patterns = []
         patterns.append(self.pattern())
-        while self.peek() != ')':
+        while self.peek() != TokenKind.RParen:
             patterns.append(self.pattern())
-
-        self.expect(')')
+        self.expect(TokenKind.RParen)
         return (not_terminal_name, patterns)
 
     # pattern = number 
     def pattern(self):
-        if self.peek() == 'number': 
-            self.expect('number')
-            return 'number' 
+        tokenkind, tokenvalue = self.peekv()
+        
+        if tokenkind == TokenKind.Ident:
+            if tokenvalue == 'number': 
+                self.expect(TokenKind.Ident)
+                return 'number'
+            if tokenvalue == 'variable-not-otherwise-mentioned':
+                self.expect(TokenKind.Ident)
+                return 'variable-not-otherwise-mentioned'
 
-        if self.peek() == 'variable-not-otherwise-mentioned':
-            self.expect('variable-not-otherwise-mentioned')
-            return 'variable-not-otherwise-mentioned'
-
-
-        if self.peek() == '(':
+        if tokenkind == TokenKind.LParen:
             return self.pattern_sequence()
 
-        ident = self.peek()
-
-        if is_number(ident): return Literal(ident, LiteralKind.Number)
-        if is_string(ident): return Literal(ident, LiteralKind.String)
-
-        print(ident, is_string(ident))
-        if is_boolean(ident): return Literal(ident, LiteralKind.Boolean)
+        if tokenkind == TokenKind.Integer: 
+            ident = self.expect(tokenkind)
+            return Literal(ident, TokenKind.Integer)
+        if tokenkind == TokenKind.Decimal: 
+            ident = self.expect(tokenkind)
+            return Literal(ident, TokenKind.Decimal)
+        if tokenkind == TokenKind.String : 
+            ident = self.expect(tokenkind)
+            return Literal(ident, TokenKind.String)
+        if tokenkind == TokenKind.Boolean: 
+            ident = self.expect(tokenkind)
+            return Literal(ident, TokenKind.Boolean)
         else:
-            return self.consume_ident()
+            # not an obvious case, could be either non-terminal or literal,
+            # need a set of non-terminals to decide.
+            self.expect(TokenKind.Ident)
+            return tokenvalue 
 
 
     # pattern-sequence : ( pattern(_id)? (literal ...)?  )
     # FIXME perhaps (_id) should be applied outside pattern_sequence?
     def pattern_sequence(self):
-        self.expect('(')
-
+        self.expect(TokenKind.LParen)
         sequence = []
-        while self.peek() != ')':
+        while self.peek() != TokenKind.RParen:
             pat = self.pattern()
+            
+            """
             underscore = pat.find('_')
             if underscore != -1:
                 ident = pat[underscore:]
@@ -271,16 +240,21 @@ class RedexSpecParser:
                 sequence.append(pat[underscore:])
             else:
                 sequence.append(pat)
-
-            if self.peek() == '...':
-                self.expect('...')
-                sequence.append('...')
-        self.expect(')')
+            """
+            sequence.append(pat)
+            tokenkind, tokenvalue = self.peekv()
+            if tokenvalue == '...':
+                self.expect(TokenKind.Ident)
+                sequence.append(tokenvalue)
+        self.expect(TokenKind.RParen)
         return sequence
 
     def parse(self):
-        self.expect('(')
-        if self.peek() == 'define-language':
+        self.expect(TokenKind.LParen)
+
+        tokenkind, tokenvalue = self.peekv()
+        
+        if tokenvalue == 'define-language':
             return self.define_language()
 
 
@@ -288,7 +262,7 @@ class RedexSpecParser:
 
 
 
-#print(RedexSpecParser("test2.rkt").parse())
+print(RedexSpecParser("test2.rkt").parse())
     
 
 
